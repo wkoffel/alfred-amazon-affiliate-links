fba1824e6a1dc63d448e487bdd33ea39b52cd4b5
Will Koffel <will@koffel.org>
2019-10-27 10:34:41 -0400

    moved to JSON format preferred for Alfred 3+, support for macOS catalina

--- amazon-affiliate-code.rb
+++ amazon-affiliate-code.rb
@@ -1,6 +1,6 @@
 require 'rubygems' unless defined? Gem
 require './bundle/bundler/setup'
-require "alfredo"
+require "./alfred-helper.rb"
 require "./code.rb"
 
 def console_log(msg)
@@ -15,18 +15,18 @@ if(args[0] == "__setcode")
   Code.write(new_code)
   puts new_code
 else
-  workflow = Alfredo::Workflow.new
+  workflow = Alfred::Workflow.new
   new_code = args[0]
   if(!new_code || new_code.empty?)
     current_code = Code.read
     if(!current_code || current_code.empty?)
-      workflow << Alfredo::Item.new(:valid => false, :title => "Set Affiliate Code", :subtitle => "Provide Amazon Tag (e.g. 'clear0e4-20')", :icon_path => "icon.png")
+      workflow << Alfred::Item.new(:valid => false, :title => "Set Affiliate Code", :subtitle => "Provide Amazon Tag (e.g. 'clear0e4-20')", :icon_path => "icon.png")
     else
-      workflow << Alfredo::Item.new(:valid => false, :title => "Current Affiliate Code is '#{current_code}'", :icon_path => "icon.png")
+      workflow << Alfred::Item.new(:valid => false, :title => "Current Affiliate Code is '#{current_code}'", :icon_path => "icon.png")
     end
   else
     # set the new code
-    workflow << Alfredo::Item.new(:arg=> "__setcode #{new_code}", :title => "Set Affiliate Code", :subtitle => "to '#{new_code}'", :icon_path => "icon.png")
+    workflow << Alfred::Item.new(:arg=> "__setcode #{new_code}", :title => "Set Affiliate Code", :subtitle => "to '#{new_code}'", :icon_path => "icon.png")
   end
   workflow.output!
 end
--- amazon-affiliate-links.rb
+++ amazon-affiliate-links.rb
@@ -1,6 +1,6 @@
 require 'rubygems' unless defined? Gem
 require './bundle/bundler/setup'
-require "alfredo"
+require "./alfred-helper.rb"
 require "addressable/uri"
 require "./code.rb"
 
@@ -9,7 +9,7 @@ def console_log(msg)
   `logger -t 'Alfred Workflow' '#{escape[msg]}'`
 end
 
-workflow = Alfredo::Workflow.new
+workflow = Alfred::Workflow.new
 
 def create_affiliate_link(product_url, code)
   uri = Addressable::URI.parse(product_url)
@@ -24,13 +24,13 @@ return nil if ARGV[0].match("code")
 product_url = ARGV[0]
 aff_code = Code.read
 if(!aff_code || aff_code.empty?)
-  workflow << Alfredo::Item.new(:valid => false, :title => "No Affiliate Code Set", :subtitle => "Run 'alink code your-code-tag' to setup", :icon_path => "icon.png")
+  workflow << Alfred::Item.new(:valid => false, :title => "No Affiliate Code Set", :subtitle => "Run 'alink code your-code-tag' to setup", :icon_path => "icon.png")
 elsif(!product_url || product_url.empty?)
-  workflow << Alfredo::Item.new(:valid => false, :title => "Generate Link", :subtitle => "Provide Product URL", :icon_path => "icon.png")
+  workflow << Alfred::Item.new(:valid => false, :title => "Generate Link", :subtitle => "Provide Product URL", :icon_path => "icon.png")
 elsif(new_url = create_affiliate_link(product_url, aff_code))
-  workflow << Alfredo::Item.new(:arg => new_url, :title => "Generate Link", :subtitle => "with Amazon Affiliate Code", :icon_path => "icon.png")
+  workflow << Alfred::Item.new(:arg => new_url, :title => "Generate Link", :subtitle => "with Amazon Affiliate Code", :icon_path => "icon.png")
 else
-  workflow << Alfredo::Item.new(:valid => false, :title => "Invalid URL", :subtitle => "Provide Product URL", :icon_path => "icon.png")
+  workflow << Alfred::Item.new(:valid => false, :title => "Invalid URL", :subtitle => "Provide Product URL", :icon_path => "icon.png")
 end
 
 workflow.output!
